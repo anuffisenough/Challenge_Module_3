ee210b5ee96695069e1d5b60fa7aff3fa4cd1373
anuffisenough <uffmanch@gmail.com>
2022-12-10 13:37:49 -0700

    adds initial prompts

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  window.prompt("Please choose a password length from 8 to 128 characters.", "")
+  window.prompt("Should the password include lowercase letters? Y or N")
+  window.prompt("Should the password include uppercase letters? Y or N")
+  window.prompt("Should the password include numeric values? Y or N")
+  window.prompt("Should the password include special characters? Y or N")
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
