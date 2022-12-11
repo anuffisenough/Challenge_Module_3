c2dceb978486cbcad843f82d0d8586f5752c95f0
anuffisenough <uffmanch@gmail.com>
2022-12-10 17:20:08 -0700

    adds first validation function

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,15 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
  
-  passwordLength = window.prompt("Please choose a password length from 8 to 128 characters.", "")
-    console.log(passwordLength);
+  var passwordLength = window.prompt("Please choose a password length from 8 to 128 characters.", "");
+  var expression1 = (passwordLength >= 8);
+  var expression2 = (passwordLength <= 128);  
 
-  window.alert("Thank you!")
+  if(expression1 && expression2){
+    window.alert("Thank you!");
+  } else {window.alert("Please enter a valid password length from 8 to 128 characters.");}
+
+/*
 
   passwordLowerCase = window.prompt("Should the password include lowercase letters? Y or N")
 
@@ -25,6 +30,8 @@ function writePassword() {
 
   window.alert("Please view your password below")
 
+  */
+
   var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
@@ -37,6 +44,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-/*var passwordLength = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var passwordLength = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var lowerCaseLetters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-var upperCaseLetters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]*/
+var upperCaseLetters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
