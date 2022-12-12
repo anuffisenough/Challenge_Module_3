2c1fa4bd7f92babf3c8b7a96b2b4dcc8f38e8641
anuffisenough <uffmanch@gmail.com>
2022-12-12 06:36:11 -0700

    adds arrays for characters

--- Develop/script.js
+++ Develop/script.js
@@ -4,23 +4,25 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
  
-  var passwordLength = window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
+  var passwordLengthInput = window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
+    window.alert("Thank you!");
+
+  var passwordCaseInput = window.prompt("Should the password include uppercase letters? Y or N", "")
 
     window.alert("Thank you!")
 
-  var passwordCase = window.prompt("Should the password include uppercase letters? Y or N", "")
+  var passwordNumeralsInput = window.prompt("Should the password include numeric values? Y or N")
 
     window.alert("Thank you!")
 
-  var passwordNumerals = window.prompt("Should the password include numeric values? Y or N")
+  var passwordSpecialCharacterInput = window.prompt("Should the password include special characters? Y or N")
 
     window.alert("Thank you!")
 
-  var passwordSpecialNumberals = window.prompt("Should the password include special characters? Y or N")
-
-    window.alert("Thank you!")
-
-    
+  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
   window.alert("Please view your password below")
 
@@ -29,7 +31,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 
 }
 
