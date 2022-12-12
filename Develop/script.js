324bd6246737f9eb1140691878d915833f6448dc
anuffisenough <uffmanch@gmail.com>
2022-12-11 21:03:23 -0700

    removes individual validations

--- Develop/script.js
+++ Develop/script.js
@@ -6,27 +6,19 @@ function writePassword() {
  
   var passwordLength = window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
 
-  expression1 = (passwordLength >= 8);
-  expression2 = (passwordLength <= 128);
+  window.alert("Thank you!")
 
-  if (expression1 && expression2) {
-    window.alert("Thank you!")
-  } else {
-    window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
-    window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
-    window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
-    window.alert("Thank you!");
-  }
-
-  passwordUpperCase = window.prompt("Should the password include uppercase letters? Y or N", "")
+  var passwordCase = window.prompt("Should the password include uppercase letters? Y or N", "")
 
   window.alert("Thank you!")
 
-  passwordNumerals = window.prompt("Should the password include numeric values? Y or N")
+  var passwordNumerals = window.prompt("Should the password include numeric values? Y or N")
 
   window.alert("Thank you!")
 
-  passwordSpecialCharacters = window.prompt("Should the password include special characters? Y or N")
+  var passwordSpecialNumberals = window.prompt("Should the password include special characters? Y or N")
+
+  window.alert("Thank you!")
 
   window.alert("Please view your password below")
 
