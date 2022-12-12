28f8042a19379bea4ad226b6916192c440aa21d8
anuffisenough <uffmanch@gmail.com>
2022-12-11 20:13:37 -0700

    cleans up prompts for minimal criteria

--- Develop/script.js
+++ Develop/script.js
@@ -4,17 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
  
-  var passwordLength = window.prompt("Please choose a password length from 8 to 128 characters.", "");
-  var expression1 = (passwordLength >= 8);
-  var expression2 = (passwordLength <= 128);  
-
-  if(expression1 && expression2){
-    window.alert("Thank you!");
-  } else {window.alert("Please enter a valid password length from 8 to 128 characters.");}
-
-/*
-
-  passwordLowerCase = window.prompt("Should the password include lowercase letters? Y or N")
+  passwordLength = window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
 
   window.alert("Thank you!")
 
@@ -30,7 +20,7 @@ function writePassword() {
 
   window.alert("Please view your password below")
 
-  */
+
 
   var password = generatePassword();
 
@@ -44,6 +34,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var passwordLength = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var lowerCaseLetters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-var upperCaseLetters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
