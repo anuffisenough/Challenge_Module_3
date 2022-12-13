70fa5111a08cb16be114d85b947ad886ec9c5c5c
anuffisenough <uffmanch@gmail.com>
2022-12-12 21:07:00 -0700

    updates prompts

--- script.js
+++ script.js
@@ -1,35 +1,82 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 // Write password to the #password input
 function writePassword() {
- 
-  var passwordLengthInput = window.prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
-  var expression1 = (passwordLengthInput >= 8);
-  var expression2 = (passwordLengthInput <= 128);
+  
+  var length = prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
+  if (8 <= length && length <= 128) {
+    alert("Thank you!");
+  } else {
+    prompt("Please choose a valid password length as a whole number from 8 to 128 characters.", "");
+  };
 
-  if (expression1 && expression2) {
-    window.alert("Thank you!");
-  } else if (!expression1 || !expression2) {
-    window.prompt("Please choose a valid password length as a whole number from 8 to 128 characters.", "");
-    window.alert("Thank you!");
-  }
+  var upperCase = prompt("Should the password include upper case letters? Y or N")
+  if (upperCase == "y" || upperCase == "n") {
+    alert("Thank you!");
+  } else if (upperCase != "y" && upperCase != "n") {
+    prompt("Please enter a valid response. Should the password include upper case letters? Y or N", "");
+    alert("Thank you!");
+  };
 
-  var passwordCaseInput = window.prompt("Should the password include uppercase letters? Y or N", "")
-  if (passwordCaseInput == "y" || passwordCaseInput == "n") {
-    window.alert("Thank you!");
-  } else if (passwordCaseInput != "y" && passwordCaseInput != "n") {
-    window.prompt("Should the password include uppercase letters? Y or N", "");
-    window.alert("Thank you!");
-  }
+  var lowerCase = prompt("Should the password include lower case letters? Y or N")
+  if (lowerCase == "y" || lowerCase == "n") {
+    alert("Thank you!");
+  } else if (lowerCase != "y" && lowerCase != "n") {
+    prompt("Please enter a valid response. Should the password include lower case letters? Y or N", "");
+    alert("Thank you!");
+  };
 
+  var numbers = prompt("Should the password include numbers? Y or N")
+  if (numbers == "y" || numbers == "n") {
+    alert("Thank you!");
+  } else if (numbers != "y" && numbers != "n") {
+    prompt("Please enter a valid response. Should the password include numbers? Y or N", "");
+    alert("Thank you!");
+  };
+
+  var special = prompt("Should the password include special characters? Y or N")
+  if (special == "y" || special == "n") {
+    alert("Thank you!");
+  } else if (special != "y" && special != "n") {
+    prompt("Please enter a valid response. Should the password include special characters? Y or N", "");
+    alert("Thank you!");
+  };
+  
+  /*
+  var upperCase = confirm("The password should include upper case letters.")
+  if (upperCase) {
+    alert("Thank you!");
+  };
+
+  var lowerCase = confirm("The password should include lower case letters.")
+  if (lowerCase) {
+    alert("Thank you!");
+  };
+
+  var numbers = confirm("The password should include numbers.")
+  if (numbers) {
+    alert("Thank you!");
+  };
+
+  var special = confirm("The password should include special characters.")
+  if (special) {
+    alert("Thank you!");
+  };
+
+  /*
   var passwordNumeralsInput = window.prompt("Should the password include numbers? Y or N")
   if (passwordNumeralsInput == "y" || passwordNumeralsInput == "n") {
     window.alert("Thank you!");
   } else if (passwordNumeralsInput != "y" && passwordNumeralsInput != "n") {
     window.prompt("Should the password include numbers? Y or N", "");
     window.alert("Thank you!");
-  }
+  };
+
   var passwordSpecialCharacterInput = window.prompt("Should the password include special characters? Y or N")
   if (passwordSpecialCharacterInput == "y" || passwordSpecialCharacterInput == "n") {
     window.alert("Thank you!");
@@ -37,11 +84,8 @@ function writePassword() {
     window.prompt("Should the password include special characters? Y or N", "");
     window.alert("Thank you!");
   }
-  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-
+  
+*/
   window.alert("Please view your password below")
 
   var password = generatePassword();
