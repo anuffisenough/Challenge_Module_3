c219e6b06fca7adaa5928bf74368e0473dfccc47
anuffisenough <uffmanch@gmail.com>
2022-12-27 17:13:07 -0700

    adds conditions for functions

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ var passwardArray3 = [];
 
 
 function writePassword() {
-//Determines password lenght and iteration number. Prompts to re-enter if outside of specified range.
+//Determines password length and iteration number. Prompts to re-enter if outside of specified range.
   var length = prompt("Please choose a password length as a whole number from 8 to 128 characters.", "");
   if (8 <= length && length <= 128) {
     alert("Thank you!");
   } else {
     prompt("Please choose a valid password length as a whole number from 8 to 128 characters.", "");
+    alert("Thank you!");
   };
 
 //User input determines upper case inclusion and start of password character array. Validates input to get appropriate y or n answer.
@@ -63,9 +64,43 @@ function writePassword() {
   };
 
 //Unable to finalize array iteration to generate password.
+
   alert("Please view your password below")
 
   var password = generatePassword();
+
+  function generatePassword() {
+    if (writePassword.upperCase == "y", writePassword.lowercase == "n", writePassword.numbers == "n", writePassword.special == "n"){
+      //put function for only uppercase letters here;
+  } else if (writePassword.upperCase == "y", writePassword.lowercase == "y", writePassword.numbers == "n", writePassword.special == "n"){
+      //put function for only upper and lowercase letters here;
+  } else if (writePassword.upperCase == "y", writePassword.lowercase == "y", writePassword.numbers == "y", writePassword.special == "n"){
+      //put function for both case letters and numbers here;
+  } else if (writePassword.upperCase == "y", writePassword.lowercase == "y", writePassword.numbers == "y", writePassword.special == "y") {
+    //put funtion for all types of characters here;
+  } else if (writePassword.upperCase == "n", writePassword.lowercase == "y", writePassword.numbers == "n", writePassword.special == "n") {
+    //put function for only lowercase letters here;
+  } else if (writePassword.upperCase == "n", writePassword.lowercase == "y", writePassword.numbers == "y", writePassword.special == "n"){
+    //put function for only lowercase letters and numbers here;
+  } else if (writePassword.upperCase == "n", writePassword.lowercase == "y", writePassword.numbers == "y", writePassword.special == "y") {
+    //put function for both case letters, numbers, and special characters here;
+  } else if (writePassword.upperCase == "n", writePassword.lowercase == "n", writePassword.numbers == "n", writePassword.special == "y") {
+  //put function for only special characters here;
+  } else if (writePassword.upperCase == "y", writePassword.lowercase == "n", writePassword.numbers == "n", writePassword.special == "y") {
+  //put function for only uppercase letters and special characters here;
+  } else if (writePassword.upperCase == "y", writePassword.lowercase == "n", writePassword.numbers == "y", writePassword.special == "n"){
+    //put function for only uppercase and number letters here;
+  } else if (writePassword.upperCase == "y", writePassword.lowercase == "y", writePassword.numbers == "n", writePassword.special == "y"){
+    //put function for both case letters and special characters here;
+  } else if (writePassword.upperCase == "n", writePassword.lowercase == "y", writePassword.numbers == "n", writePassword.special == "y") {
+  //put funtion for lowercase and special here;
+  } else if (writePassword.upperCase == "n", writePassword.lowercase == "n", writePassword.numbers == "y", writePassword.special == "n") {
+  //put function for only numbers here;
+  } else if (writePassword.upperCase == "n", writePassword.lowercase == "n", writePassword.numbers == "y", writePassword.special == "y"){
+  //put function for only lowercase letters and numbers here;
+  } else (writePassword.upperCase == "n", writePassword.lowercase == "y", writePassword.numbers == "y", writePassword.special == "n") {
+  //put function for both case letters, numbers, and special characters here;
+  };
 
   var passwordText = document.querySelector("#password");
 
